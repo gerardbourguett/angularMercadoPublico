38c9ed1556df81c72bde9f273c8eb22ca8f788e6
Gerard Bourguett <gerard.bourguett@gmail.com>
2021-06-15 14:04:18 -0400

    Commit cambios 14/06

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { TablaComponent } from './tabla/tabla.component';
 import { VermasComponent } from './vermas/vermas.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { DateAgoPipe } from './pipes/date-ago.pipe';
+import { LeftbarComponent } from './leftbar/leftbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TablaComponent,
     VermasComponent,
-    HomeComponent
+    HomeComponent,
+    DateAgoPipe,
+    LeftbarComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
